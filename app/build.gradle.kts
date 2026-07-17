6311bcab8221c7ef464a4fafc94cdaecb09f03b0
lurenyang <lurenyang@outlook.com>
2026-07-17 23:00:00 +0800

    release: bump Android version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     applicationId = "tech.lury.xpod"
     minSdk = 33
     targetSdk = 36
-    versionCode = 3
-    versionName = "0.2.0"
+    versionCode = 4
+    versionName = "0.3.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
