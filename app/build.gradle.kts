7cf60ad0fdb9dbc9767af19a43887905adb0fec8
lurenyang <lurenyang@outlook.com>
2026-07-24 05:24:04 +0800

    release: bump Android version to 0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     applicationId = "tech.lury.xpod"
     minSdk = 33
     targetSdk = 36
-    versionCode = 8
-    versionName = "0.7.0"
+    versionCode = 9
+    versionName = "0.8.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
